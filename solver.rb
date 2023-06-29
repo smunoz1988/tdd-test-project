514298505d6617d41b1ffbb7d994524829d825cf
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-29 15:43:26 -0500

    set TEST for reverse method on Solver class

--- solver.rb
+++ solver.rb
@@ -3,4 +3,14 @@ class Solver
         1
     end
 
+    def factorial(number) # takes one argument, an integer N, and returns the factorial for that number. The factorial is the multiplication of all integers from 1 to N and has the special case that the factorial of 0 is 1. This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception
+        if number < 0
+            raise "Number must be positive"
+        end
+        if number == 0
+            return 1
+        else
+            return number * factorial(number - 1)
+        end
+    end
 end
