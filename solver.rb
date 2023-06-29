c4fb92c8faed5a45707f3d210f791f4698ea8f34
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-29 16:45:40 -0500

    Add a negative value that raises an exception

--- solver.rb
+++ solver.rb
@@ -15,12 +15,15 @@ class Solver
   end
 
   def fizzbuzz(number)
+    raise 'Number must be positive' if number.negative?
     if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
     elsif (number % 3).zero?
       'fizz'
     elsif (number % 5).zero?
       'buzz'
+    else
+      number
     end
   end
 end
