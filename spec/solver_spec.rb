12bcba36fb4471a3e5e607e5f351370f61855b54
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-29 12:21:15 -0500

    Test for initialize Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,10 @@
 require './solver'
 
 describe Solver do
+    describe "#solve" do # test for initialize class Solver
+        it "returns 1 for 1" do
+            solver = Solver.new
+            expect(solver.solve(1)).to eql(1)
+        end
+    end
 end
