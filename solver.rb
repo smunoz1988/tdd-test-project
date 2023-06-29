53417a8ecdb5c54ebf39354427c3fb710f57ecc2
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-29 16:46:20 -0500

    linter fix

--- solver.rb
+++ solver.rb
@@ -16,6 +16,7 @@ class Solver
 
   def fizzbuzz(number)
     raise 'Number must be positive' if number.negative?
+
     if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
     elsif (number % 3).zero?
